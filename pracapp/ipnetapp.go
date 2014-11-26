09a27971772f9ef4bad160b50951d7fda61de554
lxyxd2003m <liuxiaoyong.jd.com>
2014-11-26 11:01:22 +0800

    network

--- pracapp/ipnetapp.go
+++ pracapp/ipnetapp.go
@@ -10,6 +10,10 @@ func main() {
 	var defaultSubnetPool *net.IPNet
 
 	defaultSubnetPool = goprac.SubnetPool("10.2.3.0/32")
-	fmt.Printf("net----->%s", defaultSubnetPool.Network())
+	fmt.Printf("ip----->%s      mask----->%s\n", defaultSubnetPool.IP, defaultSubnetPool.Mask)
+
+	ip, _ := goprac.NetworkParms("192.168.1.1/24")
+
+	fmt.Printf("ip----->%s      mask----->%s", ip, ip.DefaultMask())
 
 }
--- praclib/netip.go
+++ praclib/netip.go
@@ -1,16 +1,23 @@
 package goprac
 
 import (
+	"log"
 	"net"
 )
 
 func SubnetPool(networkString string) *net.IPNet {
 
 	_, subnetPool := NetworkParms(networkString)
+	return subnetPool
 }
 
 func NetworkParms(networkString string) (net.IP, *net.IPNet) {
 
 	containerIP, subnet, err := net.ParseCIDR(networkString)
+	if err != nil {
+
+		log.Fatal("parse ip error", err)
+	}
+
 	return containerIP, subnet
 }
